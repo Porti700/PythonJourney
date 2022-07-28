2b320f48413868a414832be5c44cc7ff2323ed74
Kevin Portillo <40046770+Porti700@users.noreply.github.com>
2022-07-28 14:11:37 -0600

    Add files via upload

---
+++ day1/ej3input.py
@@ -0,0 +1,3 @@
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+x= input("Cual es tu nombre?")
+print(len(x))
